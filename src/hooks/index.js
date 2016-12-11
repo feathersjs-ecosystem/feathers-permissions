948ea3e1f348555b71a4fe359f9d196b8f1ca983
fredy.morales <fredy_morales@ftn.fedex.com>
2016-12-10 22:50:27 -0500

    squash! Export setPermissions hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
 import isPermitted from './is-permitted';
 import checkPermissions from './check-permissions';
-import setPermissions from './setPermissions.js';
+import setPermissions from './set-permissions.js';
 
 export default {
   isPermitted,
