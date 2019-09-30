bd7d7f73b1917f09b2bb5d10da8522f64479eaaf
David Luecke <daff@neyeon.com>
2019-09-29 21:33:05 -0700

    Add TypeScript definitions

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+// TypeScript Version: 3.0
+import { Hook } from '@feathersjs/feathers';
+
+export interface PermissionOptions {
+  error?: boolean;
+  entity?: string;
+  field?: string;
+  permissions: string|string[];
+}
+
+declare const checkPermissions: ((options?: PermissionOptions) => Hook);
+export default checkPermissions;
