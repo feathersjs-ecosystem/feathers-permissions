a6ed15fb27555e939d8e02ba0aa0fd06caea5ff3
Dmitriy Kovel <dkovel@scnsoft.com>
2020-04-06 19:21:50 +0300

    Update roles field type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,11 @@
 // TypeScript Version: 3.0
-import { Hook } from '@feathersjs/feathers';
+import { Hook, HookContext } from '@feathersjs/feathers';
 
 export interface PermissionOptions {
   error?: boolean;
   entity?: string;
   field?: string;
-  roles: string|string[];
+  roles: string|string[]|{(context: HookContext): string | string[]};
 }
 
 declare const checkPermissions: ((options?: PermissionOptions) => Hook);
