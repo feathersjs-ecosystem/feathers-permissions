a0397b7849d635d2045c5b05cc93363be042fef0
Mitya <d.kovel@yandex.ru>
2020-04-09 18:09:39 +0300

    Role types (#64)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,12 @@
 // TypeScript Version: 3.0
-import { Hook } from '@feathersjs/feathers';
+import { Hook, HookContext } from '@feathersjs/feathers';
 
+export type RoleList = string|string[];
 export interface PermissionOptions {
   error?: boolean;
   entity?: string;
   field?: string;
-  roles: string|string[];
+  roles: RoleList|{(context: HookContext): RoleList}|{(context: HookContext): Promise<RoleList>};
 }
 
 declare const checkPermissions: ((options?: PermissionOptions) => Hook);
